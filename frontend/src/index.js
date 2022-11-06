b91ef982e97252b39beeca671a006f07db1bf2f1
rezaul360 <rezaulkarim3162@gmail.com>
2022-11-06 12:30:12 +0600

    part 09

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,13 +4,15 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { configureStore } from "@reduxjs/toolkit";
 import { Provider } from "react-redux";
-import productsReducer from "./features/productsSlice";
+import productsReducer, { productsFetch } from "./features/productsSlice";
 
 const store = configureStore({
   reducer: {
     products: productsReducer,
   },
 });
+
+store.dispatch(productsFetch());
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
